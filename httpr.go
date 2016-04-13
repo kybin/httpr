dd104f2d045d6fca72ac08a21098ad55eddc583d
kim yongbin <kybinz@gmail.com>
2016-04-13 21:04:18 +0900

    add initial implementation

---
+++ httpr.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	b, err := ioutil.ReadFile("PORTMAP")
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Print("please make PORTMAP file.")
+			os.Exit(1)
+		}
+		log.Fatal(err)
+	}
+	s := strings.TrimRight(string(b), "\r\n")
+	ss := strings.Split(s, "\n")
+	portmap := make(map[string]string)
+	for i, l := range ss {
+		if l == "" {
+			continue
+		}
+		ll := strings.Split(l, ":")
+		if len(ll) != 2 {
+			log.Fatalf("PORTMAP: not a valid line: %v\n%v", i, l)
+		}
+		portmap[ll[0]] = ll[1]
+	}
+	fmt.Print(portmap)
+	if len(portmap) == 0 {
+		fmt.Print("not any redirection specfied. nothing to do.")
+	}
+	for prefix, port := range portmap {
+		http.HandleFunc(prefix, redirect(prefix, port))
+	}
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func redirect(prefix, port string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		req, err := http.NewRequest(r.Method, "http://localhost:"+port+"/"+strings.TrimPrefix(r.URL.Path, prefix), r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// TODO: write response header
+		t, err := template.New("resp").Parse("{{.}}")
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Execute(w, template.HTML(string(body)))
+	}
+}
